2ad7556bfcb7851a1c8c4188b2eea1e66eda18c8
Juanjo <juanjorc98@gmail.com>
2017-08-19 16:02:36 -0300

    Funciones agregadas
    Refactorizacion de conexion, filtros agregados y traer elementos
    agregado.

--- lib/db/querier.py
+++ lib/db/querier.py
@@ -8,9 +8,17 @@ class Querier(object):
     tabla = ""
     prefijo = ""
 
+    conexion = {}
+
     def __init__(self, tabla, prefijo = ""):
         self.prefijo = self.prefijo
         self.tabla = tabla
+
+        self.user = "root"
+        self.password = "admin1234"
+        self.host = "127.0.0.1"
+        self.database = "corupel"
+
 
 # Esta funcion recibe un diccionario donde key = columna y value = valor
     def insertarElemento(self, elemento):
@@ -37,14 +45,18 @@ class Querier(object):
             return
         consulta = "UPDATE {} SET ".format(self.tabla)
         donde = "WHERE "
+
+        total = len(elemento)
+
         for index, columna in enumerate(elemento.keys()):
             if "id" in columna.lower():
-                print("La columna: " + columna + " fue ignorada")
+                # print("La columna: " + columna + " fue ignorada")
+                total -= 1
                 if self.prefijo in columna.lower():
                     donde += "{}{} = %({}{})s".format(self.prefijo, columna, self.prefijo, columna)
                 continue
             consulta += "{}{} = %({}{})s".format(self.prefijo,columna,self.prefijo,columna)
-            if len(elemento)-1 != index:
+            if index < total:
                 consulta += ", "
             else:
                 consulta += "\n"
@@ -53,17 +65,65 @@ class Querier(object):
         print("\nDEBUG - Consulta actualizar elemento a mysql: ", consulta , "\n")
         self.__consultar(consulta, elemento)
 
-    def __consultar(self, consulta, elemento):
-# Debo levantar estos elementos (usr, pword, host, dbase) de un archivo .ini.
-# Si el archivo .ini no existe debo crearlo. Estos datos se deben guardar
-# de forma encriptada y luego desencriptar al usarlo.
-        db = mysql.connector.connect(
-            # user = config.usr, password = config.pword,
-            # host = config.hst, database = config.dbase
-            user = "root", password = "admin1234",
-            host = "127.0.0.1", database = "corupel"
-            )
+    def traerElementos(self, campos, condiciones = None):
+        donde = ""
+        consulta = "SELECT "
 
+        totalCampos = len(campos)
+        # totalCondiciones = len(condiciones)
+
+        if not campos:
+            consulta += "* "
+        else:
+            for index, campo in enumerate(campos):
+                consulta += "{}".format(campo)
+
+                if index != totalCampos -1:
+                    consulta += ", "
+
+        consulta += "FROM {} ".format(self.tabla)
+
+        if condiciones:
+            donde = self.__agregarFiltros(condiciones)
+        consulta += donde
+
+        print("DEBUG - CONSULTA PARA VER ELEMENTOS: ", consulta)
+
+        db = self.__conectar()
+        cursor = db.cursor()
+
+        respuesta = cursor.fetchall()
+
+        print (respuesta)
+
+        cursor.close()
+        db.close()
+
+        return respuesta
+
+    def __agregarFiltros(self, filtros):
+        donde = "\nWHERE "
+
+        totalFiltros = len(filtros)
+
+        for index, filtro in enumerate(filtros):
+            campo, condicion, valor = filtro
+            donde += "{} {} {}".format(campo, condicion, valor)
+            # campo + condicion + valor
+            if index < totalFiltros:
+                donde += ", "
+        # donde += campo + condicion + "%({})s".campo
+
+        return donde
+
+    def __conectar(self):
+        con = mysql.connector.connect(
+            user = self.user, password = self.password,
+            host = self.host, database = self.database)
+        return con
+
+    def __consultar(self, consulta, elemento):
+        db = self.__conectar()
         cursor = db.cursor()
         try:
             if (type(elemento) != type({})):
