48eaa0db839b8b2fb16eafa3a4f28a95c95efc3f
Juanjo <juanjorc98@gmail.com>
2017-08-24 18:49:44 -0300

    Actualizado el inicializador
    Los archivos poblar tabla y inicio.sql estÃn al dia.

--- inicio_db.sql
+++ inicio_db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `articulos` (
-  `art_ID` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `prov_ID` int(16) UNSIGNED NOT NULL,
+  `art_id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `prov_id` int(16) UNSIGNED NOT NULL,
   `art_cod_barras` varchar(20) NULL UNIQUE KEY,
   `art_descripcion` varchar(20) NOT NULL,
   `art_marca` varchar(20) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE `articulos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `operarios` (
-  `ope_ID` int(16) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ope_id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `ope_legajo` int(16) NOT NULL,
   `ope_nombre` varchar(20) NOT NULL,
   `ope_apellido` varchar(20) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE `operarios` (
 --
 
 CREATE TABLE `proveedores` (
-  `prov_ID` int(16) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `prov_id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `prov_nombre` varchar(20) NOT NULL,
   `prov_razonsocial` varchar(20) NOT NULL,
   `prov_cuit` varchar(20),
@@ -39,40 +39,58 @@ CREATE TABLE `proveedores` (
 
 
 CREATE TABLE `agrupaciones` (
-  `ag_ID` int(16) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `ag_nombre` varchar(20) NOT NULL,
+  `ag_id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `ag_nombre` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `destino` (
-  `des_ID` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE `destinos` (
+  `des_id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `des_maquina` varchar(20) NOT NULL,
-  `des_descripcion` varchar(20),
+  `des_descripcion` varchar(20)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `stock` (
-  `stock_ID` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `art_ID` int(16) UNSIGNED NOT NULL ,
-  `ing_ID` int(16) UNSIGNED NOT NULL ,
-  -- `stock_fecha` date(20) NOT NULL, --
-  `cantidad` int(20) UNSIGNED NOT NULL,
-  `stock_costo` int(20),
+CREATE TABLE `movimientos_ingreso` (
+  `movi_id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `art_id` int(16) UNSIGNED NOT NULL ,
+  `ing_id` int(16) UNSIGNED NOT NULL ,
+  `movi_cantidad` int(20) UNSIGNED NOT NULL,
+  `movi_costo` int(20)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `ingreso` (
-  `ing_ID` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `prov_ID` int(16) UNSIGNED NOT NULL,
-  `fecha` date NOT NULL  
+CREATE TABLE `movimientos_egreso` (
+  `move_id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `art_id` int(16) UNSIGNED NOT NULL,
+  `eg_id` int(16) UNSIGNED NOT NULL,
+  `movi_id` int(16) UNSIGNED NOT NULL,
+  `move_cantidad` int(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `egreso` (
+
+
+CREATE TABLE `ingresos` (
+  `ing_id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `prov_id` int(16) UNSIGNED NOT NULL,
+  `ing_fecha` date NOT NULL  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `egresos` (
   `eg_id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `ope_id` int(16) NOT NULL,
-  
+  `eg_fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `comprobante` (
+CREATE TABLE `comprobantes` (
   `comp_id` int(16) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `comp_prefijo` varchar(10) NOT NULL,
-  `comp_numero` int(20),
+  `comp_numero` int(20) NOT NULL,
+  `comp_fecha` date NOT NULL,
   `ing_id` int(16) UNSIGNED NOT NULL 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `remitos` (
+  `rem_id` int(16) UNSIGNED NOT NULL AUTO_iNCREMENT PRIMARY KEY,
+  `rem_prefijo` varchar(10) NOT NULL,
+  `rem_numero` int(20) NOT NULL,
+  `rem_fecha` date NOT NULL,
+  `eg_id` int(16) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
