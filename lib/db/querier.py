cb7bae056ac26b263ded80430f8531af2be4d9e2
Juanjo <juanjorc98@gmail.com>
2017-08-24 14:11:50 -0300

    Libreria de consulta con DB actualizada

--- lib/db/querier.py
+++ lib/db/querier.py
@@ -39,66 +39,94 @@ class Querier(object):
         print("\nDEBUG - Consulta de insertar elemento:\n", consulta, "\n\n", elemento, "\n")
         self.__consultar(consulta, elemento)
 
-    def actualizarElemento(self, elemento):
+    def actualizarElemento(self, elemento, condiciones = None):
         if type(elemento) != type({}):
             raise TypeError("Solo se pueden actualizar elementos del tipo diccionario")
             return
+        if condiciones == None:
+            raise TypeError("No se puede actualizar un elemento sin proporcionar su id")
+            return
+
         consulta = "UPDATE {} SET ".format(self.tabla)
-        donde = "WHERE "
+
+        donde = ""
 
         total = len(elemento)
 
         for index, columna in enumerate(elemento.keys()):
-            if "id" in columna.lower():
+            if (self.prefijo + "id") in columna.lower():
                 # print("La columna: " + columna + " fue ignorada")
                 total -= 1
-                if self.prefijo in columna.lower():
-                    donde += "{}{} = %({}{})s".format(self.prefijo, columna, self.prefijo, columna)
+                donde = self.agregarFiltros(columna, " = ", columna)
+                # donde += "{}{} = %({}{})s".format(self.prefijo, columna, self.prefijo, columna)
                 continue
             consulta += "{}{} = %({}{})s".format(self.prefijo,columna,self.prefijo,columna)
-            if index < total:
+            if index < total-1:
                 consulta += ", "
             else:
                 consulta += "\n"
+
+        # if condiciones:
+        #     donde = self.__agregarFiltros(condiciones)
+        # else:
+        #     donde = self.__agregarFiltros()
         consulta += donde
 
         print("\nDEBUG - Consulta actualizar elemento a mysql: ", consulta , "\n")
         self.__consultar(consulta, elemento)
 
-    def traerElementos(self, campos, condiciones = None):
+    def existeElemento(self, elemento):
+        # consulta =
+        pass
+
+
+    def traerElementos(self, campos = None, condiciones = None, limite = None):
         donde = ""
         consulta = "SELECT "
 
-        totalCampos = len(campos)
+
         # totalCondiciones = len(condiciones)
 
         if not campos:
             consulta += "* "
         else:
+            totalCampos = len(campos)
             for index, campo in enumerate(campos):
                 consulta += "{}".format(campo)
 
                 if index != totalCampos -1:
                     consulta += ", "
 
-        consulta += "FROM {} ".format(self.tabla)
+        consulta += " FROM {} ".format(self.tabla)
 
         if condiciones:
             donde = self.__agregarFiltros(condiciones)
         consulta += donde
+
+        if limite:
+            consulta += " LIMIT {}".format(limite)
 
         print("DEBUG - CONSULTA PARA VER ELEMENTOS: ", consulta)
 
         db = self.__conectar()
         cursor = db.cursor()
 
+        cursor.execute(consulta)
         respuesta = cursor.fetchall()
 
-        print (respuesta)
+        # print (respuesta)
 
         cursor.close()
         db.close()
 
+        # listaRespuesta = [[]]
+        # for index, cosa in enumerate(respuesta):
+        #     listaRespuesta.append([])
+        #     for cosita in cosa:
+        #         listaRespuesta[index].append(cosita)
+        #
+        # print (respuesta)
+        # return listaRespuesta
         return respuesta
 
     def __agregarFiltros(self, filtros):
@@ -110,7 +138,7 @@ class Querier(object):
             campo, condicion, valor = filtro
             donde += "{} {} {}".format(campo, condicion, valor)
             # campo + condicion + valor
-            if index < totalFiltros:
+            if index < totalFiltros-1:
                 donde += ", "
         # donde += campo + condicion + "%({})s".campo
 
