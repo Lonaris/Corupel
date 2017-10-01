b5c90e58d5ff77f2fd297d4380e36afe23c04c6e
Juanjo <juanjorc98@gmail.com>
2017-10-01 17:35:33 -0300

    Limpieza del presentador

--- Presenter/PresenterPrincipal.py
+++ Presenter/PresenterPrincipal.py
@@ -1,6 +1,5 @@
 
 from PyQt5 import QtWidgets
-# from PyQt5.QtGui import
 from PyQt5.QtCore import QModelIndex, QAbstractItemModel, QRegExp
 from Vistas import VistaPrincipal
 from Presenter import PresenterProveedor, PresenterArticulo, PresenterOperario, PresenterIngreso, PresenterEgreso
@@ -24,28 +23,16 @@ class PrincipalPresenter(QtWidgets.QWidget):
 
         self.contenido = self.vista.findChild(QtWidgets.QStackedWidget)
 
-        # menu.selectionModel().currentChanged.connect(self.actualizarContenido)
-
-
         for index, pr in enumerate(self.presenters):
             if index == 3 or index == 4:
                 self.contenido.insertWidget(index, pr.vistaDetalle)
             else:
                 self.contenido.insertWidget(index, pr.vistaLista)
 
-
-
         self.vista.show()
 
         rx = QRegExp("btn_main_*")
         botones = self.vista.findChildren(QtWidgets.QPushButton, rx)
-
-        # print("\n\nLA LISTA DE BOTONES ES LA SIGUIENTE: ")
-        #
-        # for boton in botones:
-        #     texto = boton.text()
-        #     boton.clicked.connect(self.actualizarContenido(texto))
-        #     print("\n", boton.text())
 
         self.vista.btn_main_articulos.clicked.connect(self.mostrarArticulos)
         self.vista.btn_main_proveedores.clicked.connect(self.mostrarProveedores)
@@ -53,27 +40,19 @@ class PrincipalPresenter(QtWidgets.QWidget):
         self.vista.btn_main_ingresos.clicked.connect(self.mostrarIngresos)
         self.vista.btn_main_egresos.clicked.connect(self.mostrarEgresos)
 
-
-    # def actualizarContenido(self, index):
-    #     index = index.row()
-    #     self.contenido = self.vista.self.contenido
-    #
-    #     self.contenido.setCurrentIndex(index)
-    #     self.contenido.setMinimumSize(self.contenido.widget(self.contenido.currentIndex()).size())
-
         self.mostrarArticulos()
 
     def mostrarArticulos(self):
-        self.contenido.setCurrentIndex(0) # Para DEBUG. Eliminar mas tarde
+        self.contenido.setCurrentIndex(0)
 
     def mostrarProveedores(self):
-        self.contenido.setCurrentIndex(1) # Para DEBUG. Eliminar mas tarde
+        self.contenido.setCurrentIndex(1)
 
     def mostrarOperarios(self):
-        self.contenido.setCurrentIndex(2) # Para DEBUG. Eliminar mas tarde
+        self.contenido.setCurrentIndex(2)
 
     def mostrarIngresos(self):
-        self.contenido.setCurrentIndex(3) # Para DEBUG. Eliminar mas tarde
+        self.contenido.setCurrentIndex(3)
 
     def mostrarEgresos(self):
-        self.contenido.setCurrentIndex(4) # Para DEBUG. Eliminar mas tarde
+        self.contenido.setCurrentIndex(4)
